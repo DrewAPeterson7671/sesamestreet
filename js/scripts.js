a289fee1583027cd77f83439692f89597d83a697
Student <example@example.com>
2019-10-24 15:21:49 -0700

    Add functionality to match user with the sesame street muppet.

--- js/scripts.js
+++ js/scripts.js
@@ -6,13 +6,32 @@ $('document').ready(function() {
     var color = $('input#color').val();
     var birthdate = $('input#birthdate').val();
     var pet = $('select#pet').val();
+    var iceCream = $('select#iceCream').val();
 
     $('#name1Output').text(name1);
     $('#name2Output').text(name2);
     $('#colorOutput').text(color);
     $('#dateOutput').text(birthdate);
     $('#petOutput').text(pet);
+    $('#iceCreamOutput').text(iceCream);
     $('.show').removeClass('show');
-    event.preventDefault();
+
+    if (pet === "Dog" && iceCream === "Chocolate") {
+      $('.finalSesame').text("Cookie Monster");
+    } else if (pet === "Dog" && iceCream === "Strawberry") {
+      $('.finalSesame').text("Big Bird");
+    } else if (pet === "Cat" && iceCream === "Chocolate") {
+      $('.finalSesame').text("Kermit The Frog");
+    } else if (pet === "Cat" && iceCream === "Strawberry") {
+      $('.finalSesame').text("Grover");
+    } else if (pet === "Bird" && iceCream === "Chocolate") {
+      $('.finalSesame').text("Oscar The Grouch");
+    } else if (pet === "Bird" && iceCream === "Strawberry") {
+      $('.finalSesame').text("Elmo");
+    } else {
+      $('.finalSesame').text("It's Broken"); 
+    }
+
+        event.preventDefault();
   });
 });
